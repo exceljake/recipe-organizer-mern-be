9f5e76527e42362ddaafad24a00500513f8560d9
eaquino <eaquino@iripple.com>
2025-10-01 00:19:40 +0800

    include URL of local FE

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const corsOptions = {
         'https://your-frontend-domain.com',
         'https://your-frontend-domain.vercel.app'
       ]
-    : ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000'],
+    : ['http://localhost:3000', 'http://localhost:3002', 'http://127.0.0.1:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
